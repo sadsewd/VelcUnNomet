29bc2285d76a8caa6101ce7ebff6ac0c932158d7
sadsewd <[lacitiskarlis@gmail.com]>
2021-04-06 11:43:14 +0300

    Uzsākta skirpta rakstīšana

---
+++ Assets/Skripti/DragDripSkripts.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDripSkripts : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler/*, IEndDragHandler */
+{
+
+   // public Objekti objektuSkripts;
+    private CanvasGroup kanvasGroup;
+    private RectTransform velkObjectTransf;
+
+    void Awake()
+    {
+        kanvasGroup = GetComponent<CanvasGroup>();
+        velkObjectTransf = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerDown(PointerEventData notikums) 
+    {
+        Debug.Log("klikskis");
+    }
+
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Iesākts vilkt objektu");
+        // objektuSkripts.pedejaisVilktais = null;
+        kanvasGroup.alpha = 0.6f;
+        kanvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData notikums)
+    {
+        Debug.Log("Notiek vilkšana!");
+       // velkObjectTransf.anchoredPosition += notikums.delta / objektuSkripts.kanva.scaleFactor;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # VelcUnNomet
 ## Veicamo darbu saraksts:
 - [x] izveidot GitHub repo
-- [ ] sagatavot Unity projektu darba uzākšanai (mapes, bildes, skaņas)
+- [x] sagatavot Unity projektu darba uzākšanai (mapes, bildes, skaņas)
 - [ ] Realizēt objektu vilkšanu un nomešanu (uz kartes, nepareizi, pareizi)
 - [ ]  Realizēt objektu transformāciju (Rotācija, izmērs)
 - [ ] Realizēt laika atskaiti un uzvaras logu (laiks, zvaigznītes)
